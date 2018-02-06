906772fb33792bd2fba380f661deb396446e9dc6
Antonio Citro <acitro96@gmail.com>
2018-02-06 10:16:22 +0100

    aggiunta ordercontrol.java

---
+++ src/admin/OrderControl.java
@@ -0,0 +1,54 @@
+package admin;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import code.CartModel;
+
+/**
+ * Servlet implementation class OrderControl
+ */
+@WebServlet("/OrderControl")
+public class OrderControl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	static CartModel modelCart = new CartModel();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public OrderControl() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			request.setAttribute("carrelli", modelCart.getAllStorico());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/admin/order.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
